a5772cbe4eade8e88b187dddf0e3b0b2d0b5acde
Stilwell <100096883+Stilwelll@users.noreply.github.com>
2024-05-21 15:14:17 -0400

    library app v1
    initial library app commit

---
+++ C# Projects/Library App/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_App
+{
+    class Book
+    {
+        public string title;
+        public string category;
+        public int pageCount;
+        
+        public Book(string atitle, string acategory, int apageCount)
+        {
+            atitle = title;
+            acategory = category;
+            apageCount = pageCount;
+        }
+    }
+}
---
+++ C# Projects/Library App/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Library_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool state = true;
+            string answer;
+            int answerNum;
+
+            while (state)
+            {
+                Console.WriteLine("Welcome to my Primitive Library App.");
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.WriteLine("Please pick from the options below.");
+                Console.WriteLine("1). Check Inventory");
+                Console.WriteLine("2). Enter a new book");
+                Console.WriteLine("3). Check out a book");
+                Console.WriteLine();
+            }
+        }
+    }
+}
